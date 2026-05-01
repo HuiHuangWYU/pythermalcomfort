062ebd25c1a3c404460b31ccd5fa5c848488e7e8
Federico Tartarini <{{ .email }}>
2026-05-01 17:54:31 +1000

    Bump version: 3.9.4rc1 → 3.9.4rc2

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = "pythermalcomfort"
 year = "2025"
 author = "Federico Tartarini"
 project_copyright = f"{year}, {author}"
-version = release = "3.9.4rc1"
+version = release = "3.9.4rc2"
 
 autodoc_typehints = "none"
 pygments_style = "trac"
